2c8d5bc8f86e83e17fd163a1e473ff06cbd75570
SUGAN-2007 <developer.sugan@gmail.com>
2025-09-21 17:58:52 +0530

    remove

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -8,15 +8,14 @@ export default function Nav() {
                     <button><img src="/public/icons8-menu-50.png" alt="Menu" /></button>
                 </div> */}
                 <div className='header'>
-                        <img src="/public/Untitled design(1).png" alt="" />
+                    <img src="/public/Untitled design(1).png" alt="" />
                     <h1>Urbanly</h1>
                 </div>
-                <div className='cart'>
-                    <button><img src="/public/icons8-cart-100(1).png" alt="cart" /></button>
+                <div className='cart'> 
+                    <button><img src="/public/icons8-cart-100(1).png" alt="cart" /></button> 
                     <button><img src="/public/icons8-account-100.png" alt="account" /></button>
-                </div>
-                
+                </div> 
             </nav>
-     </>      
+        </>
     )
 }
