27cef45a41933ea54ac641e257a08b72a0014530
SUGAN-2007 <developer.sugan@gmail.com>
2025-09-21 19:47:04 +0530

    nav done

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,21 +1,21 @@
 import '../css/nav.css'
 
 export default function Nav() {
-    return (
-        <>
-            <nav>
-                {/* <div className="Menu">
-                    <button><img src="/public/icons8-menu-50.png" alt="Menu" /></button>
-                </div> */}
-                <div className='header'>
-                    <img src="/public/Untitled design(1).png" alt="" />
-                    <h1>Urbanly</h1>
-                </div>
-                <div className='cart'> 
-                    <button><img src="/public/icons8-cart-100(1).png" alt="cart" /></button> 
-                    <button><img src="/public/icons8-account-100.png" alt="account" /></button>
-                </div> 
-            </nav>
-        </>
-    )
+        return (
+                <>
+                        <nav className='main'>
+                                <div className='header'>
+                                        <img src="/public/image/logo-trans.png" alt="" />
+                                        <h1>Urbanly</h1>
+                                </div>
+
+                                <div className='cart'>
+                                        <button> <img src="/public/image/notification.png" alt="Notification" /></button>
+                                        <button><img src="/public/image/troll-bl.png" alt="Delivery" /></button>
+                                        <button><img src="/public/image/cart.png" alt="Cart" /></button>
+                                        <button><img src="/public/image/account.png" alt="Account" /></button>
+                                </div>
+                        </nav>
+                </>
+        )
 }
