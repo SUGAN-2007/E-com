e24a9239b4e3097ad2c5a178ceac519f92b7c4be
SUGAN-2007 <developer.sugan@gmail.com>
2025-09-21 13:41:30 +0530

    logo

---
+++ src/app.jsx
@@ -0,0 +1,12 @@
+import Nav from "./components/nav"
+import './css/app.css'
+
+function App() {
+        return (
+                <main>
+                        <Nav />
+                </main>
+        )
+}
+
+export default App
---
+++ src/components/nav.jsx
@@ -0,0 +1,22 @@
+import '../css/nav.css'
+
+export default function Nav() {
+    return (
+        <>
+            <nav>
+                {/* <div className="Menu">
+                    <button><img src="/public/icons8-menu-50.png" alt="Menu" /></button>
+                </div> */}
+                <div className='header'>
+                        <img src="/public/Untitled design(1).png" alt="" />
+                    <h1>Urbanly</h1>
+                </div>
+                <div className='cart'>
+                    <button><img src="/public/icons8-cart-100(1).png" alt="cart" /></button>
+                    <button><img src="/public/icons8-account-100.png" alt="account" /></button>
+                </div>
+                
+            </nav>
+     </>      
+    )
+}
