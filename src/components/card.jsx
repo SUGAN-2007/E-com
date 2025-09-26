eda3a90d1b899b60eae9b619e104eec4a9930ca2
SUGAN-2007 <developer.sugan@gmail.com>
2025-09-26 14:36:43 +0530

    nav and data

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,21 +1,8 @@
 import Cart from "./cart";
 import Nav from "./nav";
 import '../css/card.css'
+import {shirts} from '../data'
 export default function Products() {
-        const shirts = [
-                { no: 1, name: "Classic White Shirt", image: "/products/1.webp", rs: "$25.00" },
-                { no: 2, name: "Casual Blue Shirt", image: "/products/2.webp", rs: "$28.00" },
-                { no: 3, name: "Formal Black Shirt", image: "/products/3.webp", rs: "$35.00" },
-                { no: 4, name: "Checked Red Shirt", image: "/products/4.webp", rs: "$30.00" },
-                { no: 5, name: "Denim Shirt", image: "/products/5.webp", rs: "$40.00" },
-                { no: 6, name: "Linen Summer Shirt", image: "/products/6.webp", rs: "$32.00" },
-                { no: 7, name: "Slim Fit Grey Shirt", image: "/products/7.webp", rs: "$38.00" },
-                { no: 8, name: "Printed Hawaiian Shirt", image: "/products/8.webp", rs: "$29.00" },
-                { no: 9, name: "Olive Green Shirt", image: "/products/9.webp", rs: "$34.00" },
-                { no: 10, name: "Brown Flannel Shirt", image: "/products/10.webp", rs: "$36.00" },
-                { no: 11, name: "Sky Blue Polo Shirt", image: "/products/11.webp", rs: "$27.00" },
-        ];
-
         return (
                 <main>
                         <p>Shirts</p>
@@ -28,7 +15,7 @@ export default function Products() {
                                                 <div>
                                                         <p>{item.name}</p>
                                                         <p>{item.rs}</p>
-                                                        <p>{item.des}</p>
+                                                        {/* <p>{item.des}</p> */}
                                                         <div className="numberofproducts">
                                                                 <button>-</button>
                                                                 <p>0</p>
--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -3,22 +3,22 @@ import '../css/nav.css'
 export default function Nav() {
         return (
                 <>
-                        <nav className='main'>
-                                <div className='header'>
-                                        <img className='logo' src="/image/logo-trans.png" alt="" />
-                                        {/* <h1 className='head'>Urbanly</h1> */}
+                        <nav className='nav-main'>
+                                <div className='nav-header'>
+                                        <img className='nav-logo' src="/image/logo-trans.png" alt="" />
+                                        <h1 className='nav-head'>Urbanly</h1>
                                 </div>
 
-                                <div className='cart'>
+                                <div className='nav-cart'>
                                         {/* <button className='but'><img className='image' src="/image/notification.png" alt="Notification" /></button>
                                         <button className='but'><img className='image' src="/image/troll-bl.png" alt="Delivery" /></button>
                                         <button className='but'><img className='image' src="/image/cart.png" alt="Cart" /></button>
                                         <button className='but'><img className='image' src="/image/account.png" alt="Account" /></button> */}
-                                        <a className='link' href="">Home</a>
-                                        <a className='link' href="">Notification</a>
-                                        <a className='link' href="">Delivery</a>
-                                        <a className='link' href="">Cart</a>
-                                        <a className='link' href="">Login/Sign up</a>
+                                        <a className='nav-link' href="">Home</a>
+                                        <a className='nav-link' href="">Notification</a>
+                                        <a className='nav-link' href="">Delivery</a>
+                                        <a className='nav-link' href="">Cart</a>
+                                        <a className='nav-link' href="">Login/Sign up</a>
 
                                 </div>
                         </nav>
