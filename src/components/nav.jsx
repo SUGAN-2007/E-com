30ee43b2f1990a88ca7ce6167af49c682e31e711
SUGAN-2007 <developer.sugan@gmail.com>
2025-09-26 13:17:57 +0530

    nav done

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -6,14 +6,20 @@ export default function Nav() {
                         <nav className='main'>
                                 <div className='header'>
                                         <img className='logo' src="/image/logo-trans.png" alt="" />
-                                        <h1 className='head'>Urbanly</h1>
+                                        {/* <h1 className='head'>Urbanly</h1> */}
                                 </div>
 
                                 <div className='cart'>
-                                        <button className='but'><img className='image' src="/image/notification.png" alt="Notification" /></button>
+                                        {/* <button className='but'><img className='image' src="/image/notification.png" alt="Notification" /></button>
                                         <button className='but'><img className='image' src="/image/troll-bl.png" alt="Delivery" /></button>
                                         <button className='but'><img className='image' src="/image/cart.png" alt="Cart" /></button>
-                                        <button className='but'><img className='image' src="/image/account.png" alt="Account" /></button>
+                                        <button className='but'><img className='image' src="/image/account.png" alt="Account" /></button> */}
+                                        <a className='link' href="">Home</a>
+                                        <a className='link' href="">Notification</a>
+                                        <a className='link' href="">Delivery</a>
+                                        <a className='link' href="">Cart</a>
+                                        <a className='link' href="">Login/Sign up</a>
+
                                 </div>
                         </nav>
                 </>
